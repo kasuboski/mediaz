22111daddc23a606473f3f9883ccf36f1470ecb3
Josh Kasuboski <josh.kasuboski@gmail.com>
2024-08-07 17:40:27 -0500

    problematic this passes

---
+++ pkg/client/client_test.go
@@ -0,0 +1,32 @@
+package client_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/kasuboski/mediaz/pkg/client"
+)
+
+func TestClient_CanCall(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte("{}"))
+	}))
+	defer server.Close()
+	hc := server.Client()
+
+	c, err := client.NewClientWithResponses(server.URL, client.WithHTTPClient(hc))
+	if err != nil {
+		t.Fatalf("couldn't create client: %v", err)
+	}
+
+	resp, err := c.ConfigurationDetailsWithResponse(context.TODO())
+	if err != nil {
+		t.Fatalf("failed to get config: %v", err)
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		t.Fatalf("expected 200, got %d", resp.StatusCode())
+	}
+}
