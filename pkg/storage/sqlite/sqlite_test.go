2ab3abe3caadc904a760a598f7be5bb564f78daa
Josh Kasuboski <josh.kasuboski@gmail.com>
2024-09-20 16:17:13 -0500

    add sqlite test for indexers

---
+++ pkg/storage/sqlite/sqlite_test.go
@@ -0,0 +1,62 @@
+package sqlite
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kasuboski/mediaz/pkg/storage"
+	"github.com/kasuboski/mediaz/pkg/storage/sqlite/schema/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	store := initSqlite(t, context.Background())
+	assert.NotNil(t, store)
+}
+
+func TestIndexerStorage(t *testing.T) {
+	ctx := context.Background()
+	store := initSqlite(t, ctx)
+	assert.NotNil(t, store)
+
+	ix, err := store.ListIndexers(ctx)
+	assert.Nil(t, err)
+	assert.Empty(t, ix)
+
+	apikey := "supersecret"
+	create := model.Indexers{
+		ID:       1,
+		Name:     "Index",
+		Priority: 20,
+		URI:      "http://here",
+		ApiKey:   &apikey,
+	}
+	res, err := store.CreateIndexer(ctx, create)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, res)
+
+	ix, err = store.ListIndexers(ctx)
+	assert.Nil(t, err)
+	assert.Len(t, ix, 1)
+	actual := ix[0]
+	assert.Equal(t, &create, actual)
+
+	err = store.DeleteIndexer(ctx, int64(actual.ID))
+	assert.Nil(t, err)
+
+	ix, err = store.ListIndexers(ctx)
+	assert.Nil(t, err)
+	assert.Empty(t, ix)
+}
+
+func initSqlite(t *testing.T, ctx context.Context) storage.Storage {
+	store, err := New(":memory:")
+	assert.Nil(t, err)
+
+	schemas, err := storage.ReadSchemaFiles("../../../schema.sql")
+	assert.Nil(t, err)
+
+	err = store.Init(ctx, schemas...)
+	assert.Nil(t, err)
+	return store
+}
